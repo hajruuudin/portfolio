8829a45a9f72daf5d16e7b94ce2b173eab387e48
hajruuudin <hajruuudin@gmail.com>
2024-12-09 22:25:16 +0100

    Updates

--- js/homepage.js
+++ js/homepage.js
@@ -1,28 +1,34 @@
+if (window.innerWidth > 768) { 
+    document.addEventListener("scroll", () => {
+        const scrollableElement = document.scrollingElement || document.documentElement;
+        const scrollPosition = scrollableElement.scrollTop;
+
+        const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
+
+        const image1 = document.getElementById("image1");
+        const image2 = document.getElementById("image2");
+        const image3 = document.getElementById("image3");
+        const imagebg = document.getElementById("imagebg");
+        const header = document.getElementById("header");
+
+        image1.style.transform = `translateY(${clamp(scrollPosition * 0.7, -200, 200)}px)`;
+        image2.style.transform = `translateY(${clamp(scrollPosition * 0.5, -200, 200)}px)`;
+        image3.style.transform = `translateY(${clamp(scrollPosition * 0.3, -200, 200)}px)`;
+        imagebg.style.transform = `translateY(${clamp(scrollPosition * 0.1, -100, 100)}px)`;
+
+        header.style.transform = `translateY(${clamp(scrollPosition * 0.3, -100, 100)}px)`;
+        header.style.opacity = `${1 - scrollPosition * 0.002}`;
+        imagebg.style.opacity = `${1 - scrollPosition * 0.001}`;
+    });
+}
+
 document.addEventListener("scroll", () => {
-    const scrollableElement = document.scrollingElement || document.documentElement;
-    const scrollPosition = scrollableElement.scrollTop;
-
-    const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
-
-    const image1 = document.getElementById("image1");
-    const image2 = document.getElementById("image2");
-    const image3 = document.getElementById("image3");
-    const imagebg = document.getElementById("imagebg");
-    const header = document.getElementById("header");
     const navbar = document.getElementById("navbarContainer");
-
-    image1.style.transform = `translateY(${clamp(scrollPosition * 0.7, -200, 200)}px)`;
-    image2.style.transform = `translateY(${clamp(scrollPosition * 0.5, -200, 200)}px)`;
-    image3.style.transform = `translateY(${clamp(scrollPosition * 0.3, -200, 200)}px)`;
-    imagebg.style.transform = `translateY(${clamp(scrollPosition * 0.1, -100, 100)}px)`;
-
-    header.style.transform = `translateY(${clamp(scrollPosition * 0.3, -100, 100)}px)`;
-    header.style.opacity = `${1 - scrollPosition * 0.002}`;
-    imagebg.style.opacity = `${1 - scrollPosition * 0.001}`;
 
     if (window.scrollY > navbar.scrollHeight) {
         navbar.style.transform = `translateY(-120px)`;
     } else {
         navbar.style.transform = `translateY(120px)`;
     }
-});
+})
+
