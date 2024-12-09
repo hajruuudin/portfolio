dddd8d44881d3e7455240a6c68f9b9254426f0a8
hajruuudin <hajruuudin@users.noreply.github.com>
2024-12-09 19:13:30 +0000

    deploy: bbde14abb3bef148db6b3de8f736c28ffd0d09d9

---
+++ js/homepage.js
@@ -0,0 +1,25 @@
+document.addEventListener("scroll", () => {
+    const scrollableElement = document.scrollingElement || document.documentElement;
+    const scrollPosition = scrollableElement.scrollTop;
+
+    const image1 = document.getElementById("image1");
+    const image2 = document.getElementById("image2");
+    const image3 = document.getElementById("image3");
+    const header = document.getElementById("header");
+    const imagebg = document.getElementById("imagebg");
+    const navbar = document.getElementById("navbarContainer");
+
+    image1.style.transform = `translateY(${scrollPosition * 0.7}px)`;
+    image2.style.transform = `translateY(${scrollPosition * 0.5}px)`;
+    image3.style.transform = `translateY(${scrollPosition * 0.3}px)`;
+    imagebg.style.transform = `translateY(${scrollPosition * 0.1}px)`;
+    header.style.transform = `translateY(${scrollPosition * 0.3}px)`;
+    header.style.opacity = `${1 - scrollPosition * 0.002}`;
+    imagebg.style.opacity = `${1 - scrollPosition * 0.001}`;
+    
+    if(window.scrollY > navbar.scrollHeight){
+        navbar.style.transform = `translateY(-120px)`
+    } else {
+        navbar.style.transform = `translateY(120px)`
+    }
+});
